5859a6801c353c89b2f979337d7db3cf06061738
joaquim oliveira <154386267+joaquimoliveira22@users.noreply.github.com>
2025-04-29 11:43:34 -0300

    bug_fixes01

--- leitor_de_pastas/main.py
+++ leitor_de_pastas/main.py
@@ -4,15 +4,12 @@ import shutil
 def copiar_arquivos(pasta_origem, pasta_destino):
     
     
-    # Verifica se a pasta de destino existe, se não, cria
     if not os.path.exists(pasta_destino):
         os.makedirs(pasta_destino)
         print(f"Pasta '{pasta_destino}' criada com sucesso!")
     
-    # Lista todos os arquivos na pasta de origem
     arquivos = os.listdir(pasta_origem)
     
-    # Contador de arquivos copiados, cada arquivo contado, ele add 1+ ao contador
     contador = 0
     
     
@@ -20,8 +17,7 @@ def copiar_arquivos(pasta_origem, pasta_destino):
         caminho_origem = os.path.join(pasta_origem, arquivo)
         caminho_destino = os.path.join(pasta_destino, arquivo)
         
-        # Verifica se é um arquivo (não uma pasta) e move o arquivo para a pasta de origem,
-        #se colocar 'shutil.copy' ele copia os arquivos e nao move
+      
         if os.path.isfile(caminho_origem):
             shutil.move(caminho_origem, caminho_destino)
             contador += 1   
@@ -30,12 +26,10 @@ def copiar_arquivos(pasta_origem, pasta_destino):
     print(f"\nOperação concluída! {contador} arquivos foram copiados.")
 
 if __name__ == "__main__":
-    # deixar fixo a pasta onde os arquivos estao e as pastas onde deseja colocar os arquivos
-    #obs* eles serao (movidos)
+   
     origem = 'pastaparaler'
     destino = 'fortes'
     
-    # Verifica se a pasta de origem existe.
     if os.path.exists(origem):
         copiar_arquivos(origem, destino)
    
